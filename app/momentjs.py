f6e74132ecfa2495f3f312e7718f3a7ed646d2b6
David Lavelle <dlavelle7@yahoo.ie>
2016-03-26 17:09:31 +0000

    pep8ed views.py.

--- app/momentjs.py
+++ app/momentjs.py
@@ -1,8 +1,8 @@
-# Moment.js wrapper class, invoked from the template
-
 from jinja2 import Markup
 
+
 class momentjs:
+    """Moment.js wrapper class, invoked from the template"""
 
     def __call__(self, *args):
         return self.format(*args)
@@ -10,11 +10,16 @@ class momentjs:
     def __init__(self, timestamp):
         self.timestamp = timestamp
 
-    # Render method does not return a string, but instead wraps the
-    # string inside a Markup object provided by Jinja2. 
-    # This tells Jinja2 that the string should not be escaped
     def render(self, format):
-        return Markup("<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
+        """Render Markup
+
+        Render method does not return a string, but instead wraps the string
+        inside a Markup object provided by Jinja2. This tells Jinja2 that the
+        string should not be escaped
+        """
+        return Markup(
+            "<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (
+                self.timestamp.strftime("%Y-%m-%dT%H:%M:%S Z"), format))
 
     def format(self, fmt):
         return self.render("format(\"%s\")" % fmt)
@@ -24,4 +29,3 @@ class momentjs:
 
     def fromNow(self):
         return self.render("fromNow()")
-
